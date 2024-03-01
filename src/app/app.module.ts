9e021d0864c83d81a42070b34916ae8572907969
Thapelo Mokotelakoena <thapeloghothini@gmail.com>
2024-03-01 09:53:40 +0200

    About to add contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
 import { SphereComponent } from './components/three-dimensions/sphere/sphere.component';
 import { BoxComponent } from './components/three-dimensions/box/box.component';
+import { ProjectComponent } from './popups/project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     SphereComponent,
-    BoxComponent
+    BoxComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { ProjectComponent } from 'src/app/popups/project/project.component';
 
 @Component({
   selector: 'app-landing',
@@ -6,4 +8,13 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
+  projects: any = [{name: "My Portfolio",nodejs: true,angular: true,mongodb: true},
+  {name: "Likes Meter Flicks",nodejs: true,angular: false,mongodb: false}]
+
+  constructor(private dialog: MatDialog){}
+
+  showProject(): void {
+    this.dialog.open(ProjectComponent,{
+    })
+  }
 }
